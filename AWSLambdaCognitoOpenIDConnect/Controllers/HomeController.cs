8b4f4e9f7c12fb3e0b28927ea17d95a8daa0a022
Mike Bentzen <mike.bentzen@gmail.com>
2018-10-06 13:00:09 +1000

    OpenID Connect with Amazon Cognito. Additionally converted Razor Pages to MVC project.

---
+++ AWSLambdaCognitoOpenIDConnect/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSLambdaCognitoOpenIDConnect.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return SignOut(new string[] { CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme });
+        }
+    }
+}
--- AWSLambdaCognitoOpenIDConnect/Startup.cs
+++ AWSLambdaCognitoOpenIDConnect/Startup.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,13 +28,38 @@ namespace AWSLambdaCognitoOpenIDConnect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            var clientSecret = Configuration["AmazonCognito:ClientSecret"];
+            var clientId = Configuration["AmazonCognito:ClientId"];
+            var metadataAddress = Configuration["AmazonCognito:MetaDataAddress"];
+            var logOutUrl = Configuration["AmazonCognito:LogOutUrl"];
+            var baseUrl = Configuration["AmazonCognito:BaseUrl"];
+            
+            services.AddAuthentication(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options => options.Events.OnSigningIn = FilterGroupClaims)
+            .AddOpenIdConnect(options =>
+            {
+                options.ResponseType = "code";
+                options.MetadataAddress = metadataAddress;
+                options.ClientId = clientId;
+                options.ClientSecret = clientSecret;
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.Scope.Add("email");
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = logOutUrl;
+                        logoutUri += $"?client_id={clientId}&logout_uri={baseUrl}";
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+                        return Task.CompletedTask;
+                    }
+                };
             });
-
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -47,11 +77,33 @@ namespace AWSLambdaCognitoOpenIDConnect
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes => {
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        //Remove all the claims that are unrelated to our identity
+        private static Task FilterGroupClaims(CookieSigningInContext context)
+        {
+            var principal = context.Principal;
+            if (principal.Identity is ClaimsIdentity identity)
+            {
+                var unused = identity.FindAll(GroupsToRemove).ToList();
+                unused.ForEach(c => identity.TryRemoveClaim(c));
+            }
+            return Task.FromResult(principal);
+        }
+
+        private static bool GroupsToRemove(Claim claim)
+        {
+            string[] _groupObjectIds = new string[] { "identities" };
+            return claim.Type == "groups" && !_groupObjectIds.Contains(claim.Type);
         }
     }
 }
